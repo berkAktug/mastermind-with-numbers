45e881ade37128b48e30e698c80a9f40a3ccb8e2
Berk Aktuğ <38779485+berkAktug@users.noreply.github.com>
2020-10-15 08:29:34 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Threading;
+
+namespace mastermind_with_numbers
+{
+
+    class Program
+    {
+        static List<string> answers;
+        static int answerSize;
+        static int bulls;
+        static int cows;
+        static string guess;
+        static IEnumerable<string> Permutations(int size, string str)
+        {
+            if (size > 0)
+            {
+                foreach (string s in Permutations(size - 1, str))
+                    foreach (char n in str)
+                        if (!s.Contains(n))
+                            yield return s + n;
+            }
+            else
+                yield return "";
+        }
+
+        static IEnumerable<T> Shuffle<T>(IEnumerable<T> enumerable)
+        {
+            var r = new Random();
+            return enumerable.OrderBy(x => r.Next()).ToList();
+        }
+
+        static IEnumerable<T> Shuffle_Parallel<T>(IEnumerable<T> enumerable)
+        {
+            var r = new Random();
+            return enumerable.AsParallel().OrderBy(x => r.Next()).ToList();
+        }
+
+        static bool ReadBullsCows(out int bulls, out int cows)
+        {
+            string[] input = Console.ReadLine().Split(' ').ToArray();
+            bulls = cows = 0;
+            if (input.Length < 2)
+                return false;
+            else
+                return int.TryParse(input[0], out bulls)
+                    && int.TryParse(input[1], out cows);
+        }
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Bulls and Cows");
+
+            string[] line = Console.ReadLine().Split(' ').ToArray();
+
+            int.TryParse(line[0], out int upperLimit);
+            int.TryParse(line[1], out answerSize);
+            int.TryParse(line[2], out int maxTurn);
+            long.TryParse(line[3], out long maxTime);
+
+            answers = new List<string>();
+
+            string str = "";
+            for (int i = 0; i <= upperLimit; i++)
+            {
+                str += i;
+            }
+
+            answers = Permutations(answerSize, str).ToList();
+
+            answers.RemoveAll(x => x.StartsWith("0"));
+            answers = Shuffle(answers).ToList();
+
+            Stopwatch sw = new Stopwatch();
+            TimeSpan ts = new TimeSpan();
+            for (int turnCount = 0; answers.Count > 1; turnCount++)
+            {
+                guess = answers[0];
+                Console.Write(guess + " ");
+
+                if (!ReadBullsCows(out bulls, out cows))
+                    Console.WriteLine("Please check your inputs.");
+                else
+                {
+                    sw.Restart();
+
+                    if (RunWithTimeout(AlgorithmLoop, TimeSpan.FromMilliseconds(maxTime * 10000)))
+                    {}
+                    else
+                    {
+                        Console.WriteLine("Time Limit exceeded.");
+                        
+                    }
+                    if(bulls == answerSize){
+                        return 0;
+                    }
+                    sw.Stop();
+                    ts = sw.Elapsed;
+                }
+                if (turnCount > maxTurn)
+                {
+                    Console.WriteLine("Turn limit exceeded.");
+                    return 3;
+                }
+            }
+            if (answers.Count == 1)
+            {
+                Console.WriteLine("Hooray! The answer is {0}!", answers[0]);
+                return 0;
+            }
+            else
+                Console.WriteLine("No possible answer fits the scores you gave.");
+            return 0;
+        }
+
+        static bool RunWithTimeout(ThreadStart threadStart, TimeSpan timeout)
+        {
+            Thread workerThread = new Thread(threadStart);
+
+            workerThread.Start();
+
+            bool finished = workerThread.Join(timeout);
+            if (!finished)
+                workerThread.Interrupt();
+
+            return finished;
+        }
+
+        static void AlgorithmLoop()
+        {
+            for (int a = 0; a < answers.Count - 1; a++)
+            {
+                int tmpBulls = 0, tmpCows = 0;
+                for (int ix = 0; ix < answerSize; ix++)
+                    if (answers[a][ix] == guess[ix])
+                        tmpBulls++;
+                    else if (answers[a].Contains(guess[ix]))
+                        tmpCows++;
+                if ((tmpBulls != bulls) || (tmpCows != cows))
+                    answers.RemoveAt(a);
+            }
+        }
+    }
+}
